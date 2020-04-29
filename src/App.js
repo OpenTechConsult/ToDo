1e69c098677813bfb4feb65c0c1f542354c2a8e7
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-29 12:10:46 +0000

    Redefining method `render()` using the fat arrow function

--- src/App.js
+++ src/App.js
@@ -14,16 +14,14 @@ export default class App extends Component {
     })
   }
 
-  render() {
-    return (
+  render = () =>
       <div>
-        <h4 className="bg-primary text-white text-center p-2">
-          { this.state.userName }'s To Do List
-        </h4>
-        <button className="btn btn-primary m-2" onClick={ this.changeStateData }>
-          Change
-        </button>
+          <h4 className="bg-primary text-white text-center p-2">
+            { this.state.userName }'s To Do List
+          </h4>
+          <button className="btn btn-primary m-2" onClick={ this.changeStateData }>
+            Change
+          </button>
       </div>
-    )
-  }
+
 }
