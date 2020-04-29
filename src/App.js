933f6d59bf43d7a72ad2f150a76d2d67840d6cb6
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-29 13:56:11 +0000

    Adding To-Do Application features
    1- We add additional state data and expressions
    
    2- We add a input  to add new items. the form has two attributes:
    	* value that sets the input value to whatever is in the state.newItemText state variable
    	* onChange which capture an onChange event and execute the component function updateNewTextValue
    
    3- We also add a button with the onCick attribute to capture the onClick event and execute the createNewTodo method
    	* the method add a new todo item if the todo item text is not already in the todoItems array

--- src/App.js
+++ src/App.js
@@ -3,25 +3,56 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { userName: "Sandro"}
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "Sandro",
+            todoItems: [
+                { action: "Buy Flowers", done: false },
+                { action: "Get Shoes", done: false },
+                { action: "Collect Tickets", done: true },
+                { action: "Get Shoes", done: false },
+            ],
+            newItemText: ""
+        }
+    }
 
-  changeStateData = () => {
-    this.setState({
-      userName: this.state.userName === "Sandro" ? "Elom"  : "Sandro"
-    })
-  }
+    changeStateData = () => {
+        this.setState({
+            userName: this.state.userName === "Sandro" ? "Elom" : "Sandro"
+        })
+    }
 
-  render = () =>
-      <div>
-          <h4 className="bg-primary text-white text-center p-2">
-            { this.state.userName }'s To Do List
-          </h4>
-          <button className="btn btn-primary m-2" onClick={ this.changeStateData }>
-            Change
-          </button>
-      </div>
+    updateNewTextValue = (event) => {
+        this.setState({ newItemText: event.target.value});
+    }
+
+    createNewTodo = () => {
+        if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+            this.setState({
+                todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false}],
+                newItemText: ""
+            });
+        }
+    }
+
+    render = () =>
+        <div>
+            <h4 className = "bg-primary text-white text-center p-2" > 
+                { this.state.userName }'s To Do List 
+                ({this.state.todoItems.filter(t => !t.done).length}) items to do
+            </h4>
+            <button className = "btn btn-primary m-2" onClick = { this.changeStateData } >
+                Change 
+            </button>
+            <div className = "container-fluid" >
+                <div className = "my-1" >
+                    <input className="form-control" value={ this.state.newItemText } 
+                            onChange={this.updateNewTextValue}/>
+                    <button className="btn btn-primary mt-1" onClick={ this.createNewTodo }>Add</button>
+
+                </div> 
+            </div> 
+        </div>
 
 }
